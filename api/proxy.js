ae908323440d42b92c1330622d749892f927ce43
l406054531 <406054531@qq.com>
2022-05-16 20:58:28 +0800

    提交

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
-  let target = ''
+  let target = 'http://47.106.221.76:3000/'
 
-  if (req.url.startswith('/api')) {
-    //这里填目标地址
-    target = 'http://47.106.221.76:3000/'
-  }
+  // if (req.url.startswith('/api')) {
+  //   //这里填目标地址
+    
+  // }
   //创建代理对象并转发请求
   createProxyMiddleware({
     target,
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       //通过路径重写，去除请求路径中的`/ api '
       //例如/ api/user/login将被转发到 http: / / target/user/ login
-      '^/ api/ ': '/'
+      '^/api/ ': '/'
     }
   })(req, res)
 }
